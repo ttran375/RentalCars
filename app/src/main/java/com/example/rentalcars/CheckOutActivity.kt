8963be8744bc1d7737c19a8dd2ae54df168cdde4
ttran375 <ttran375@my.centennialcollege.ca>
2024-06-24 10:26:52 -0400

    Add check out view

--- app/src/main/java/com/example/rentalcars/CheckOutActivity.kt
+++ app/src/main/java/com/example/rentalcars/CheckOutActivity.kt
@@ -1,5 +1,41 @@
 package com.example.rentalcars
 
-class CheckOutActivity {
 
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+
+class CheckOutActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_check_out)
+
+
+        val selectedCar = intent.getStringExtra("SELECTED_CAR")
+        val selectedCarText: TextView = findViewById(R.id.selectedCarText)
+        selectedCarText.text = "Selected Car: $selectedCar"
+
+
+        val rentalDaysEditText: EditText = findViewById(R.id.rentalDaysEditText)
+        val checkoutButton: Button = findViewById(R.id.checkoutButton)
+
+
+        checkoutButton.setOnClickListener {
+            val rentalDays = rentalDaysEditText.text.toString().toIntOrNull()
+            if (rentalDays != null) {
+                val intent = Intent(this, PaymentActivity::class.java)
+                intent.putExtra("RENTAL_DAYS", rentalDays)
+                intent.putExtra("SELECTED_CAR", selectedCar)
+                startActivity(intent)
+            } else {
+                rentalDaysEditText.error = "Please enter a valid number of days"
+            }
+        }
+    }
 }
---
+++ app/src/main/java/com/example/rentalcars/PaymentActivity.kt
@@ -0,0 +1,5 @@
+package com.example.rentalcars
+
+class PaymentActivity {
+
+}

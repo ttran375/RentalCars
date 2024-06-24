4e9aeab1854f7ea4825c7f6f45b78b77347b873a
ttran375 <ttran375@my.centennialcollege.ca>
2024-06-24 10:21:43 -0400

    Add type view

--- app/src/main/java/com/example/rentalcars/CarTypeActivity.kt
+++ app/src/main/java/com/example/rentalcars/CarTypeActivity.kt
@@ -1,5 +1,36 @@
 package com.example.rentalcars
 
-class CarTypeActivity {
 
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+
+
+class CarTypeActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_car_type)
+
+
+        val coupeButton: Button = findViewById(R.id.coupeButton)
+        val hatchbackButton: Button = findViewById(R.id.hatchbackButton)
+        val sedanButton: Button = findViewById(R.id.sedanButton)
+        val suvButton: Button = findViewById(R.id.suvButton)
+
+
+        coupeButton.setOnClickListener { openCarList("Coupe") }
+        hatchbackButton.setOnClickListener { openCarList("Hatchback") }
+        sedanButton.setOnClickListener { openCarList("Sedan") }
+        suvButton.setOnClickListener { openCarList("SUV") }
+    }
+
+
+    private fun openCarList(carType: String) {
+        val intent = Intent(this, CarListActivity::class.java)
+        intent.putExtra("CAR_TYPE", carType)
+        startActivity(intent)
+    }
 }
